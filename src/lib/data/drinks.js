bdae93edb043e62393ea7c93d045f959a3316161
aKoolKev <kevindong0212@gmail.com>
2025-10-28 23:07:44 -0500

    Inital commit. Created project. Begin creating drinks data.

---
+++ src/lib/data/drinks.js
@@ -0,0 +1,41 @@
+export const holidayDrinks_2025_hot = [
+    {
+        name: "Caramel Brulee Latte",
+        nameCode: "",
+
+        steps: ["Espresso", "Sauce", "Milk", "Whipped cream", "Topping"],
+
+        espresso: "Default",
+        espressoAmount: ['1','2','3','4'],
+
+        sauce: "Caramel Brulee",
+        sauceAmount: ['2','3','4','5'],
+
+        milk: "2%",
+        whippedCreamed: true,
+        
+        topping: "Caramel Brulee",
+        toppingAmount: "3"
+    }
+];
+
+export const holidayDrinks_2025_iced = [
+    {
+        name: "Iced Caramel Brulee Latte",
+        nameCode: "",
+
+        steps: ["Espresso", "Sauce", "Milk", "Whipped cream", "Topping"],
+
+        espresso: "Default",
+        espressoAmount: ['1','2','3'],
+
+        sauce: "Caramel Brulee",
+        sauceAmount: ['3','4','6'],
+
+        milk: "2%",
+        whippedCreamed: true,
+        
+        topping: "Caramel Brulee",
+        toppingAmount: "3"
+    }
+];
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+
+<script>
+    import {holidayDrinks_2025_hot} from "$lib/data/drinks.js"
+</script>
+
+<main class="text-white">
+    <h1 class="font-bold uppercase text-3xl">Green Apron Academy</h1>
+    <p>{holidayDrinks_2025_hot[0].name}</p>
+</main>
+
