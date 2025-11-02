2922c8137d93f10cfb103f34bd3b9e79dcb10e60
aKoolKev <kevindong0212@gmail.com>
2025-11-02 00:53:31 -0500

    Finish adding all drink data.

--- src/lib/data/drinks.js
+++ src/lib/data/drinks.js
@@ -3,20 +3,110 @@ export const holidayDrinks_2025_hot = [
         name: "Caramel Brulee Latte",
         nameCode: "",
 
-        steps: ["Espresso", "Sauce", "Milk", "Whipped cream", "Topping"],
+        steps: ["espresso", "sauce", "milk", "whipped cream", "topping"],
 
         espresso: "Default",
-        espressoAmount: ['1','2','3','4'],
+        espressoAmount: [1,2,3,4],
 
         sauce: "Caramel Brulee",
-        sauceAmount: ['2','3','4','5'],
+        sauceAmount: [2,3,4,5],
 
         milk: "2%",
         whippedCreamed: true,
         
         topping: "Caramel Brulee",
-        toppingAmount: "3"
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint Mocha",
+        steps: ["espresso", "espressoAmount", "syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        espresso: "Default",
+        espressoAmount: [1,2,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [2,3,4,5],
+        sauce: "Mocha",
+        sauceAmount: [2,3,4,5],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint Hot Chocolate",
+        steps: ["syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        syrup: "Peppermint",
+        syrupAmount: [2,3,4,5],
+        sauce: "Mocha",
+        sauceAmount: [2,3,4,5],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint White Chocolate Mocha",
+        steps: ["espresso", "espressoAmount", "syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        espresso: "Default",
+        espressoAmount: [1,2,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [2,3,4,5],
+        sauce: "White Chocolate Mocha",
+        sauceAmount: [2,3,4,5],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint White Hot Chocolate",
+        steps: ["syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        syrup: "Peppermint",
+        syrupAmount: [2,3,4,5],
+        sauce: "White Chocolate Mocha",
+        sauceAmount: [2,3,4,5],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Gingerbread Latte",
+        steps: ['espresso', 'espressoAmount', 'syrup', 'syrupAmount', 'milk', 'whippedCream', 'topping', 'toppingAmount'],
+        espresso: 'Default',
+        espressoAmount: [1,1,2,2],
+        syrup: "Gingerbread",
+        syrupAmount: [2,3,4,5],
+        milk: '2%',
+        topping: 'Pumpkin Spice',
+        toppingAmount: 3
+    },
+    {
+        name: "Gingerbread Chai",
+        steps: ['concentrate', 'concentrateAmount', 'syrup', 'syrupAmount', 'milk', 'milkAmount', 'hotWater', 'hotWaterAmount', 'topping', 'toppingAmount'],
+        concentrate: "Chai",
+        concentrateAmount: [2,3,4,6],
+        syrup: "Gingerbread",
+        syrupAmount: [2,3,4,5],
+        milk: "2%",
+        milkAmount: "1/2",
+        hotWater: true,
+        hotWaterAmount: "1/2",
+        topping: "Pumpkin Spice",
+        toppingAmount: 3
+    },
+    {
+        name: "Sugar Cookie Latte",
+        steps: ['espresso', 'espressoAmount', 'syrup', 'syrupAmount', 'milk', 'topping', 'toppingAmount'],
+        espresso: 'blonde',
+        espressoAmount: [1,1,2,2],
+        syrup: 'Sugar Cookie', 
+        syrupAmount: [2,3,4,5],
+        milk: '2%',
+        topping: 'Green and Red Sprinkles',
+        toppingAmount: 3
     }
+    
+
 ];
 
 export const holidayDrinks_2025_iced = [
@@ -24,18 +114,187 @@ export const holidayDrinks_2025_iced = [
         name: "Iced Caramel Brulee Latte",
         nameCode: "",
 
-        steps: ["Espresso", "Sauce", "Milk", "Whipped cream", "Topping"],
+        steps: ["espresso", "sauce", "milk", "whippedCream", "topping"],
 
         espresso: "Default",
-        espressoAmount: ['1','2','3'],
+        espressoAmount: [1,2,3],
 
         sauce: "Caramel Brulee",
-        sauceAmount: ['3','4','6'],
+        sauceAmount: [3,4,6],
 
         milk: "2%",
-        whippedCreamed: true,
+        whippedCream: true,
         
         topping: "Caramel Brulee",
-        toppingAmount: "3"
+        toppingAmount: 3
+    }, 
+    {
+        name: "Caramel Brulee Frappuccino Blended Coffee",
+        steps: ["frapRoast", "milk", "sauce", "sauceAmount","syrupBase", "whippedCream", "topping", "toppingAmount"],
+        frapRoast: [2,3,4],
+        milk: "whole",
+        sauce: "Caramel Brulee",
+        sauceAmount: [1,2,2],
+        syrupBase: "Coffee",
+        syrupAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Caramel Brulee",
+        toppingAmount: 3
+    },
+    {
+        name: "Caramel Brulee Frappuccino Blended Creme",
+        steps: ["milk", "sauce", "sauceAmount","syrupBase", "whippedCream", "topping", "toppingAmount"],
+        milk: "whole",
+        sauce: "Caramel Brulee",
+        sauceAmount: [1,2,2],
+        syrupBase: "Creme",
+        syrupAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Caramel Brulee",
+        toppingAmount: 3
+    },
+    {
+        name: "Iced Peppermint Mocha",
+        steps: ["espresso", "espressoAmount", "syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        espresso: "Default",
+        espressoAmount: [1,2,3],
+        syrup: "Peppermint",
+        syrupAmount: [3,4,6],
+        sauce: "Mocha",
+        sauceAmount: [3,4,6],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    }, 
+    {
+        name: "Peppermint Mocha Frappuccino Blended Coffee",
+        steps: ["frapRoast", "milk", "sauce", "sauceAmount", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        frapRoast: [2,3,4],
+        milk: "whole",
+        sauce: "Mocha",
+        sauceAmount: [1,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [1,2,2],
+        syrupBase: "Coffee",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint Mocha Frappuccino Blended Creme",
+        steps: ["milk", "sauce", "sauceAmount", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        milk: "whole",
+        sauce: "Mocha",
+        sauceAmount: [1,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [1,2,2],
+        syrupBase: "Creme",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Iced Peppermint White Chocolate Mocha",
+        steps: ["espresso", "espressoAmount", "syrup", "syrupAmount", "sauce", "sauceAmount", "milk", "whippedCream", "topping", "toppingAmount"],
+        espresso: "Default",
+        espressoAmount: [1,2,3],
+        syrup: "Peppermint",
+        syrupAmount: [3,4,6],
+        sauce: "White Chocolate Mocha",
+        sauceAmount: [3,4,6],
+        milk: "2%",
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint White Chocolate Mocha Frappuccino Blended Coffee",
+        steps: ["frapRoast", "milk", "sauce", "sauceAmount", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        frapRoast: [2,3,4],
+        milk: "whole",
+        sauce: "White Chocolate Mocha",
+        sauceAmount: [1,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [1,2,2],
+        syrupBase: "Coffee",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Peppermint White Chocolate Mocha Frappuccino Blended Creme",
+        steps: ["milk", "sauce", "sauceAmount", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        milk: "whole",
+        sauce: "White Chocolate Mocha",
+        sauceAmount: [1,2,2],
+        syrup: "Peppermint",
+        syrupAmount: [1,2,2],
+        syrupBase: "Creme",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Chocolate curls",
+        toppingAmount: 3
+    },
+    {
+        name: "Iced Gingerbread Latte",
+        steps: ['espresso', 'espressoAmount', 'syrup', 'syrupAmount', 'milk', 'whippedCream', 'topping', 'toppingAmount'],
+        espresso: 'Default',
+        espressoAmount: [1,2,3],
+        syrup: "Gingerbread",
+        syrupAmount: [3,4,6],
+        milk: '2%',
+        topping: 'Pumpkin Spice',
+        toppingAmount: 3
+    },
+    {
+        name: "Iced Gingerbread Chai",
+        steps: ['coldFoam', 'concentrate', 'concentrateAmount' 'syrup', 'syrupAmount', 'milk', 'milkAmount', 'hotWater', 'hotWaterAmount', 'topping', 'toppingAmount'],
+        coldFoam: "Gingerbread Cream",
+        concentrate: 'Chai',
+        concentrateAmount: [3,4,6],
+        milk: "2%",
+        topping: "Pumpkin Spice",
+        toppingAmount: 3
+    },
+    {
+        name: "Iced Sugar Cookie Latte",
+        steps: ['espresso', 'espressoAmount', 'syrup', 'syrupAmount', 'milk', 'topping', 'toppingAmount'],
+        espresso: 'blonde',
+        espressoAmount: [1,2,3],
+        syrup: 'Sugar Cookie', 
+        syrupAmount: [3,4,6],
+        milk: '2%',
+        topping: 'Green and Red Sprinkles',
+        toppingAmount: 3
+    },
+    {
+        name: "Sugar Cookie Frappuccino Blended Coffee",
+        steps: ["frapRoast", "milk", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        frapRoast: [2,3,4],
+        milk: "whole",
+        syrup: "Sugar Cookie",
+        syrupAmount: [1,2,2],
+        syrupBase: "Coffee",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Red and Green Sprinkles",
+        toppingAmount: 3
+    },
+    {
+        name: "Sugar Cookie Frappuccino Blended Creme",
+        steps: ["milk", "syrup", "syrupAmount", "syrupBase", "syrupBaseAmount", "whippedCream", "topping", "toppingAmount"],
+        milk: "whole",
+        syrup: "Sugar Cookie",
+        syrupAmount: [1,2,2],
+        syrupBase: "Creme",
+        syrupBaseAmount: [2,3,4],
+        whippedCream: true,
+        topping: "Red and Green Sprinkles",
+        toppingAmount: 3
     }
+
 ];
