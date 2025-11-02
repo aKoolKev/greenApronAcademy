d27b03aa3bbe4b15af58398e215bf137eec71789
aKoolKev <kevindong0212@gmail.com>
2025-11-02 01:21:19 -0500

    Experimenting with generating random drinks.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 
 <script>
-    import {holidayDrinks_2025_hot} from "$lib/data/drinks.js"
+    import {holidayDrinks_2025_hot} from "$lib/data/drinks.js";
+
+    let drinkName = '';
+    
+    //Generate random number between [0, max)
+    function randNum (max) {
+        return (Math.floor(Math.random() * max));
+    }
+
+    function getDrink(){
+        drinkName = holidayDrinks_2025_hot[randNum(holidayDrinks_2025_hot.length)].name;
+    }
 </script>
 
 <main class="text-white">
     <h1 class="font-bold uppercase text-3xl">Green Apron Academy</h1>
-    <p>{holidayDrinks_2025_hot[0].name}</p>
+    <button on:click={getDrink}>Get Drink</button>
+    <div>
+        <span>{drinkName}</span>
+    </div>
 </main>
 
