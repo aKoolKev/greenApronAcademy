b98f13c2bbfca71515fa6d776843b187e852834d
aKoolKev <kevindong0212@gmail.com>
2025-11-15 18:37:40 -0600

    Added Google Analytics to track user metrics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,19 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Google tag (gtag.js): Tracking user metrics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WFVL4NB2BP"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'G-WFVL4NB2BP');
+	</script>
+
 </svelte:head>
+
 
 <main class="bg-[#006241] min-h-screen p-5">
 	{@render children?.()}
