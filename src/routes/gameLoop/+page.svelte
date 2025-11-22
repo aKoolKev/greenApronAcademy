e83b667244aaba2e1826c27978c0b037fc5f4858
aKoolKev <kevindong0212@gmail.com>
2025-11-22 02:51:33 -0600

    Created new route folder for game loop.

---
+++ src/routes/gameLoop/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Game Loop</h1>
+    <p>Should move all route/+page.svelte into here instead and have route/+page.svelte page be the game mode menu page</p>
+</main>
